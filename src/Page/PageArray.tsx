213749fa1c768b5b87635ce0c2fa8399d9e741fb
Yucel Sarikaya <berkaykata@gmail.com>
2022-12-24 23:53:20 +0300

    Array Data Structure

--- src/Page/PageArray.tsx
+++ src/Page/PageArray.tsx
@@ -1,8 +1,107 @@
-import React from "react";
-import { ArrayStore as rootStore } from "../Store/ArrayStore";
+import { ArrayStore as Store } from "../Store/ArrayStore";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import { useEffect, useState } from "react";
 
 const ArrayPage = () => {
-  return <div>{rootStore.test() + rootStore.a}</div>;
+  let [index, setIndex] = useState([1]);
+
+  useEffect(() => {}, [setIndex]);
+
+  const Reflesh = () => {
+    setIndex([...index, 1]);
+  };
+
+  return (
+    <div>
+      <Container>
+        Array
+        {Store.Array.map((element, index) => {
+          return (
+            <Col key={index}>
+              Dizinin {index} elemanı ={">"} {element}
+            </Col>
+          );
+        })}
+      </Container>
+      <br />
+      <Container>
+        <button
+          onClick={() => {
+            Reflesh();
+            Store.IsArrayPush();
+          }}
+        >
+          Array Insertion
+        </button>
+        {Store.PushArray.map((element, index) => {
+          return (
+            <Col key={index}>
+              Dizinin {index} elemanı ={">"} {element}
+            </Col>
+          );
+        })}
+      </Container>
+      <br />
+      <Container>
+        <button
+          onClick={() => {
+            Reflesh();
+            Store.IsArrayDelete();
+          }}
+        >
+          Array Deletion
+        </button>
+        {Store.DeleteArray.map((element, index) => {
+          return (
+            <Col key={index}>
+              Dizinin {index} elemanı ={">"} {element}
+            </Col>
+          );
+        })}
+      </Container>
+      <br />
+      <Container>
+        <input
+          placeholder="Search"
+          type={"number"}
+          onChange={(e) => {
+            Reflesh();
+            Store.IsArraySearch(
+              e.currentTarget.value === ""
+                ? undefined
+                : parseInt(e.currentTarget.value)
+            );
+          }}
+        />
+        <br />
+        <label>{Store.SetSearch}</label>
+      </Container>
+      <br />
+      <Container>
+        <input
+          placeholder="Update 2nd element of array"
+          type={"number"}
+          onChange={(e) => {
+            Reflesh();
+            Store.IsArrayUpdate(
+              e.currentTarget.value === ""
+                ? undefined
+                : parseInt(e.currentTarget.value)
+            );
+          }}
+        />
+        <br />
+        {Store.UpdateArray.map((element, index) => {
+          return (
+            <Col key={index}>
+              Dizinin {index} elemanı ={">"} {element}
+            </Col>
+          );
+        })}
+      </Container>
+    </div>
+  );
 };
 
 export default ArrayPage;
--- src/Store/ArrayStore.ts
+++ src/Store/ArrayStore.ts
@@ -1,8 +1,36 @@
 export class ArrayStore {
-  public static test = (): number => {
-    const a = 212;
-    return a;
+  public static IsArrayPush = () => {
+    ArrayStore.PushArray.push(Math.floor(Math.random() * 1000) + 1);
   };
 
-  public static a: number = 5;
+  public static IsArrayDelete = () => {
+    ArrayStore.DeleteArray.pop();
+  };
+
+  public static IsArraySearch = (key: number | undefined) => {
+    if (key === undefined) {
+      ArrayStore.SetSearch = "";
+      return false
+    }
+    const Isthere = ArrayStore.Array.indexOf(key);
+    if (Isthere !== -1) {
+      ArrayStore.SetSearch =
+        "The data you entered is in the " + Isthere + ".index";
+    } else {
+      ArrayStore.SetSearch = "The data you entered is not in the array";
+    }
+  };
+
+  public static IsArrayUpdate = (key: number | undefined) => {
+    if (key === undefined) {
+      return false
+    }
+    ArrayStore.UpdateArray[2] = key
+  };
+
+  public static Array: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  public static PushArray: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  public static DeleteArray: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  public static UpdateArray: any[] = [1, 2, 3, 4];
+  public static SetSearch: string | number | undefined = "";
 }
